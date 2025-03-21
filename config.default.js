c4ec58dbb8ae2017284901f81b1e0a15ada12c5d
Sefinek <contact@sefinek.net>
2025-03-21 10:03:19 +0100

    Update config.default.js

--- config.default.js
+++ config.default.js
@@ -3,7 +3,7 @@ exports.MAIN = {
 	UFW_LOG_FILE: '/var/log/ufw.log',
 	CACHE_FILE: '/tmp/ufw-abuseipdb-reporter.cache',
 	SERVER_ID: null, // The server name that will be visible in the reports (e.g., 'homeserver1'). If you don't want to define it, leave the value as null.
-	IP_REFRESH_SCHEDULE: '0 */6 * * *', // Cron: How often should the script check the IP address assigned by the ISP to prevent accidental self-reporting? Default: every 6 hours
+	IP_REFRESH_SCHEDULE: '0 */6 * * *', // CRON: How often should the script check the IP address assigned by the ISP to prevent accidental self-reporting? Default: every 6 hours
 	IPv6_SUPPORT: true, // Specifies whether the device has been assigned an IPv6 address.
 
 	// Reporting
@@ -12,7 +12,7 @@ exports.MAIN = {
 
 	// Automatic Updates
 	AUTO_UPDATE_ENABLED: true, // Do you want the script to automatically update to the latest version using 'git pull'? (true = enabled, false = disabled)
-	AUTO_UPDATE_SCHEDULE: '0 18 * * *', // Cron: Schedule for automatic script updates. Default: every day at 18:00
+	AUTO_UPDATE_SCHEDULE: '0 18 * * *', // CRON: Schedule for automatic script updates. Default: every day at 18:00
 
 	// Discord Webhooks
 	DISCORD_WEBHOOKS_ENABLED: false, // Should the script send webhooks? They will contain error reports, daily summaries related to reports, etc.
