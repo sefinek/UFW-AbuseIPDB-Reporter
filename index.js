38d151fe5104370189c772ad9bac438574dd82f7
Sefinek <contact@sefinek.net>
2025-04-10 02:32:50 +0200

    Fixes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const reportToAbuseIPDb = async (logData, categories, comment) => {
 		log(0, `Reported ${logData.srcIp} [${logData.dpt}/${logData.proto}]; ID: ${logData.id}; Categories: ${categories}; Abuse: ${res.data.abuseConfidenceScore}%`);
 		return true;
 	} catch (err) {
-		log(2, `Failed to report ${logData.srcIp} [${logData.dpt}/${logData.proto}]; ID: ${logData.id}; ${err.response?.data?.errors ? `\n${JSON.stringify(err.response.data.errors)}` : err.message}`, 0);
+		log(2, `Failed to report ${logData.srcIp} [${logData.dpt}/${logData.proto}]; ID: ${logData.id}; ${err.response?.data?.errors ? `\n${JSON.stringify(err.response.data.errors)}` : err.message}`, 1);
 		return false;
 	}
 };
