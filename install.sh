33196c6ab35330bd0d2cba13ba86f0a0d442f834
Sefinek <contact@sefinek.net>
2025-04-13 18:17:58 +0200

    Update

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ check_dependencies() {
 }
 
 # Check dependencies before proceeding
-check_dependencies curl node git
+check_dependencies curl node git ufw
 
 # Function to validate AbuseIPDB API key
 validate_token() {
