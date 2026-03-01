cec881498663acf46e73749653330229599fa085
Sefinek <contact@sefinek.net>
2026-03-01 09:54:58 +0100

    Update

--- config.default.js
+++ config.default.js
@@ -6,9 +6,9 @@ exports.MAIN = {
 	CACHE_FILE: './tmp/ufw-abuseipdb-reporter.cache',
 
 	/* --------------------------- Network --------------------------- */
-	IP_ASSIGNMENT: 'dynamic', // IP assignment type: 'static' for a fixed IP, 'dynamic' if it may change over time.
+	IP_ASSIGNMENT: 'dynamic', // 'static' for a fixed IP, 'dynamic' if it may change over time.
 	IP_REFRESH_SCHEDULE: '0 */6 * * *', // Cron schedule for checking the public IP assigned by your ISP. Used only with dynamic IPs to prevent accidental self-reporting. If IP_ASSIGNMENT is set to 'static', the script will check your IP only once.
-	IPv6_SUPPORT: true, // IPv6 support: true if the device has a globally routable address assigned by the ISP.
+	IPv6_SUPPORT: true, // true if the device has a globally routable address assigned by the ISP.
 
 	/* --------------------------- Reports --------------------------- */
 	ABUSEIPDB_API_KEY: '', // https://www.abuseipdb.com/account/api
@@ -22,7 +22,7 @@ exports.MAIN = {
 	DISCORD_WEBHOOK_ENABLED: false, // Enables sending Discord webhooks with error reports, execution status, and other events.
 	DISCORD_WEBHOOK_URL: '',
 	DISCORD_WEBHOOK_USERNAME: 'SERVER_ID', // Username shown as the message author. Use null for default. 'SERVER_ID' will resolve to this.MAIN.SERVER_ID.
-	DISCORD_USER_ID: '', // Your Discord account identifier.
+	DISCORD_USER_ID: '', // Discord User ID to ping on critical events. Leave empty to disable pinging.
 };
 
 
